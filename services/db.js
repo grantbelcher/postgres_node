9b2bf41b93fab59472a0a96a1c22d853e6cb859e
Grant Belcher <gtbelch@gmail.com>
2022-05-27 16:24:01 -0700

    creates initial database config and schema for test table

---
+++ services/db.js
@@ -0,0 +1,50 @@
+const pg = require("pg");
+
+const config = {
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  port: 5432,
+  max: 10,
+  idleTimoutMillis: 30000,
+};
+
+const pool = new pg.Pool(config);
+
+pool.on("connect", () => {
+  console.log("connected to the database");
+});
+
+const createTables = () => {
+  const schoolTable = `CREATE TABLE IF NOT EXISTS
+      students(
+        id SERIAL PRIMARY KEY,
+        student_name VARCHAR(128) NOT NULL,
+        student_age INT NOT NULL,
+        student_class VARCHAR(128) NOT NULL,
+        parent_contact VARCHAR(128) NOT NULL,
+        admission_date VARCHAR(128) NOT NULL
+      )`;
+  pool
+    .query(schoolTable)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+pool.on("remove", () => {
+  console.log("client removed");
+  process.exit(0);
+});
+
+//export pool and createTables to be accessible  from an where within the application
+module.exports = {
+  createTables,
+  pool,
+};
+
+require("make-runnable");
